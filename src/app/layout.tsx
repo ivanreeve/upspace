37567e5c2272dabb9625fb20226e8c3f740e4c36
ivanreeve <ivanreevelopez@gmail.com>
2025-10-04 15:24:40 +0800

    add garamond font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,33 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Noto_Serif } from 'next/font/google'
+import { Noto_Serif, EB_Garamond } from 'next/font/google';
 
-const crimson = Noto_Serif({
+const serif = Noto_Serif({
   weight: ['300', '400', '700'],
   subsets: ['latin'],
-  variable: '--font-serif'
+  variable: '--font-serif',
+});
+
+const garamond = EB_Garamond({
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  variable: '--font-garamond',
 });
 
 export const metadata: Metadata = {
   title: 'UpSpace',
   description: 'A marketplace and management platform for coworking spaces.',
-  icons: {
-    icon: 'favicon.svg'
-  }
+  icons: { icon: 'favicon.svg', },
 };
 
 export default function RootLayout({ children, }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={crimson.variable}>
+    <html lang="en" className={`${serif.variable} ${garamond.variable}`} >
       <body>
         {children}
       </body>
-    </html>
+    </html >
   );
 }
