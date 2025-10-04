dc254f24618e0b3c17f3998e6b4e21d3b8dcddf5
ivanreeve <ivanreevelopez@gmail.com>
2025-10-05 05:37:51 +0800

    fix faq

--- src/components/pages/LandingPage/LandingPage.Hero.tsx
+++ src/components/pages/LandingPage/LandingPage.Hero.tsx
@@ -5,7 +5,7 @@ import SignInCard from '@/components/auth/SignInCard';
 export function Hero() {
   return (
     <div className="flex h-[90vh] items-center">
-      { /* Left container */ }
+      { /* Left container */}
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-[4rem] text-center font-instrument-serif">
           Find the Perfect Space<br />for Work
@@ -15,8 +15,8 @@ export function Hero() {
         <SignInCard />
       </div>
 
-      { /* Right container */ }
-      <div className="relative flex-1 flex items-center justify-center bg-blue-300 rounded-lg h-[720px] overflow-hidden">
+      { /* Right container */}
+      <div className="relative flex-1 flex items-center justify-center bg-background rounded-lg h-[720px] overflow-hidden">
         <Image
           src="/img/hero-featured-dark.png"
           alt="Admiring the vast expanse."
